d4e8f6600df15f7d342881c34e7fb25dbc20eda8
topi0247 <karubona.ra247@gmail.com>
2024-07-03 01:32:28 +0900

    プルリクテスト２

--- src/app/(auth)/users/[uuid]/page.jsx
+++ src/app/(auth)/users/[uuid]/page.jsx
@@ -2,7 +2,7 @@ export default function User({ params: { uuid } }) {
   console.log(uuid);
   return (
     <article>
-      <h1>ユーザーページ</h1>
+      <h1>ユーザーページ : {uuid}</h1>
     </article>
   );
 }
