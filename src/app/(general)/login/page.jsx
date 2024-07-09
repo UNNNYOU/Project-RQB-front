991d43a1f55acb0fad38edfd1436ba9b1d01d43d
UNNNYOU <unnnyou@gmail.com>
2024-07-09 23:32:33 +0900

    [feature] githubでの認証ボタンを作成
    githubのアカウントを使用してoauth認証を行うためのボタンを作成しました。

--- src/app/(general)/login/page.jsx
+++ src/app/(general)/login/page.jsx
@@ -1,7 +1,23 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Login() {
+  const router = useRouter();
+
+  const handleAuth = () => {
+    try {
+      router.push(`${process.env.NEXT_PUBLIC_API_URL}/auth/github`);
+    } catch (error) {
+      console.error("リクエストエラー:", error);
+    }
+  };
+
   return (
     <article>
       <h1>ログインページ</h1>
+      <div className="mt-16 text-center">
+        <button onClick={handleAuth}>GitHubでログイン</button>
+      </div>
     </article>
   );
 }
