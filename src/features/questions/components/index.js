596984a4561ef7ffe8ad427a5c299c2729f7ec69
三浦大河 <taigav8@gmail.com>
2024-07-17 12:56:59 +0900

    ESLintの形に整形

--- src/features/questions/components/index.js
+++ src/features/questions/components/index.js
@@ -1,12 +1,12 @@
+import AnswererComment from './answererComment';
+import CommentForm from './commentForms';
+import CommentsSection from './commentsSection';
 import Pagination from "./pagination";
+import QuestionDetail from './questionDetail';
 import QuestionList from "./questionList";
 import QuestionerComment from './questionerComment';
-import AnswererComment from './answererComment';
-import CommentsSection from './commentsSection';
-import QuestionDetail from './questionDetail';
 import ResolvedStatus from './resolvedStatus';
 import UnresolvedStatus from './unresolvedStatus';
-import CommentForm from './commentForms';
 
 export {
   Pagination,
