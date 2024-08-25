c13d077b90ac052f8cd462d9b5f469969751d268
UNNNYOU <unnnyou@gmail.com>
2024-08-25 18:43:16 +0900

    [fix] aタグから関数での画面遷移への変更

--- src/app/(general)/login/page.jsx
+++ src/app/(general)/login/page.jsx
@@ -1,18 +1,22 @@
+"use client";
 import { FaGithub } from "rocketicons/fa";
 import { Settings } from "@/config";
 
 export default function Login() {
+  const handleGithhubAuth = () => {
+    window.location.href = `${Settings.API_URL}/auth/github`;
+  };
   return (
     <article>
       <h1>ログインページ</h1>
       <div className="mt-16 flex items-center justify-center">
-        <a
-          href={`${Settings.API_URL}/auth/github`}
+        <button
+          onClick={handleGithhubAuth}
           className="flex gap-2 rounded-md bg-black p-2 text-2xl text-white"
         >
           <FaGithub className="mb-1 inline-block size-8" />
           <span>GitHubでログイン</span>
-        </a>
+        </button>
       </div>
     </article>
   );
