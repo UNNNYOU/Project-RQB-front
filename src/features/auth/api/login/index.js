06d332a9dea84593fc698ecb2805840cc532c4dc
UNNNYOU <unnnyou@gmail.com>
2024-09-03 00:21:50 +0900

    [feature] localstrageに値がある場合には自動ログイン

--- src/features/auth/api/login/index.js
+++ src/features/auth/api/login/index.js
@@ -1,6 +1,5 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import { Settings } from "@/config";
 import { currentUserState } from "@/features/auth/api";
@@ -9,25 +8,28 @@ import { fetcher } from "@/lib";
 
 const Login = () => {
   const params = useSearchParams();
-  const { setAccessToken } = useAuth();
+  const { setAccessToken, getAccessToken } = useAuth();
   const setCurrentUser = useSetRecoilState(currentUserState);
 
-  useEffect(() => {
-    const token = params.get("token");
+  if (typeof window !== "undefined") {
+    const token = params.get("token") || getAccessToken();
     if (token) {
       setAccessToken(token);
-      fetcher(`${Settings.API_URL}/auth/me`).then((current_user) => {
-        setCurrentUser({
-          uuid: current_user.uuid,
-          name: current_user.name,
-          github_uid: current_user.github_uid,
-          term: current_user.term,
-          profile: current_user.profile,
-        });
-      });
+      fetcher(`${Settings.API_URL}/auth/me`).then(
+        (current_user) => {
+          setCurrentUser({
+            uuid: current_user.uuid,
+            name: current_user.name,
+            github_uid: current_user.github_uid,
+            term: current_user.term,
+            profile: current_user.profile,
+          });
+        },
+        [params, setAccessToken, setCurrentUser, getAccessToken],
+      );
+      return null;
     }
-  }, [params, setAccessToken, setCurrentUser]);
-  return null;
+  }
 };
 
 export default Login;
