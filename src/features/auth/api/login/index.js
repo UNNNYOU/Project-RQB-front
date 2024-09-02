1727b784e90ba2eb4c712aa2e6340fc322be88fb
UNNNYOU <unnnyou@gmail.com>
2024-09-03 00:03:19 +0900

    [feature]

--- src/features/auth/api/login/index.js
+++ src/features/auth/api/login/index.js
@@ -9,24 +9,27 @@ import { fetcher } from "@/lib";
 
 const Login = () => {
   const params = useSearchParams();
-  const { setAccessToken } = useAuth();
+  const { setAccessToken, getAccessToken } = useAuth();
   const setCurrentUser = useSetRecoilState(currentUserState);
 
   useEffect(() => {
-    const token = params.get("token");
-    if (token) {
-      setAccessToken(token);
-      fetcher(`${Settings.API_URL}/auth/me`).then((current_user) => {
-        setCurrentUser({
-          uuid: current_user.uuid,
-          name: current_user.name,
-          github_uid: current_user.github_uid,
-          term: current_user.term,
-          profile: current_user.profile,
+    if (typeof window !== "undefined") {
+      const token = params.get("token") || getAccessToken();
+      if (token) {
+        setAccessToken(token);
+        fetcher(`${Settings.API_URL}/auth/me`).then((current_user) => {
+          setCurrentUser({
+            uuid: current_user.uuid,
+            name: current_user.name,
+            github_uid: current_user.github_uid,
+            term: current_user.term,
+            profile: current_user.profile,
+          });
         });
-      });
+      }
     }
-  }, [params, setAccessToken, setCurrentUser]);
+  }, [params, setAccessToken, setCurrentUser, getAccessToken]);
+
   return null;
 };
 
