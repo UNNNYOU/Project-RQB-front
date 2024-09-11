357171ff98223a61291a2d537513d85a4888f0f5
UNNNYOU <unnnyou@gmail.com>
2024-09-11 23:48:00 +0900

    feature: 検索フォームの作成

--- src/components/layouts/headers/index.jsx
+++ src/components/layouts/headers/index.jsx
@@ -13,22 +13,22 @@ export default function Headers() {
       <Suspense>
         <Login />
       </Suspense>
-      <div className="flex justify-between md:container">
+      <div className="flex items-center justify-between md:container">
         <h1 className="flex items-center justify-center">
           <Link href={Routes.top} className="px-4 py-2">
             runteq&nbsp;<span className="font-semibold">overflow</span>
           </Link>
         </h1>
-        <div>
+        <div className="flex grow justify-center">
           <SearchForm />
         </div>
-        <nav className="relative w-full bg-white px-2">
-          <ul className="flex w-full items-center justify-end py-4 text-xs md:text-base">
+        <nav className="bg-white px-2">
+          <ul className="flex items-center justify-end py-4 text-xs md:text-base">
             {!currentUser.uuid ? (
               <li>
                 <Link
                   href={Routes.login}
-                  className="px-2 py-4 transition-all hover:bg-runteq-primary   hover:text-white md:px-4"
+                  className="px-2 py-4 transition-all hover:bg-runteq-primary hover:text-white md:px-4"
                 >
                   ログイン
                 </Link>
@@ -37,7 +37,7 @@ export default function Headers() {
               <li>
                 <Link
                   href={Routes.user(currentUser.uuid)}
-                  className="px-2 py-4 transition-all  hover:bg-runteq-primary hover:text-white md:px-4"
+                  className="px-2 py-4 transition-all hover:bg-runteq-primary hover:text-white md:px-4"
                 >
                   ユーザーページ
                 </Link>
