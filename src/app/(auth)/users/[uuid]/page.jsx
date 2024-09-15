92a143281733a95596a6f579a1e500cc0ff56617
topi0247 <karubona.ra247@gmail.com>
2024-09-15 21:23:44 +0900

    feat:質問一覧と回答一覧を追加

--- src/app/(auth)/users/[uuid]/page.jsx
+++ src/app/(auth)/users/[uuid]/page.jsx
@@ -1,10 +1,17 @@
-import { Profile } from "@/features/users/components";
+import { Suspense } from "react";
+import { Profile, UserQuestionList } from "@/features/users/components";
 
 export default function User({ params: { uuid } }) {
   return (
     <>
       <article>
         <Profile uuid={uuid} />
+      </article>
+
+      <article className="my-8">
+        <Suspense>
+          <UserQuestionList uuid={uuid} />
+        </Suspense>
       </article>
     </>
   );
